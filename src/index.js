da5cb970f1f5177a18538442013f93e8a885fcc6
Amir Ahkami <amir@Users-MacBook-Air.local>
2020-11-13 16:45:52 +0100

    react on github-pages

--- src/index.js
+++ src/index.js
@@ -4,12 +4,212 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+
+
+class Clock extends React.Component {
+
+	constructor(props) {
+		super(props);
+		this.state = { date: new Date()};
+	}
+
+	componentDidMount() {
+		this.ticker = setInterval(() => this.tick(), 1000);
+	}
+
+	tick() {
+		this.setState({
+			date: new Date()
+		});
+	}
+
+	render() {
+		return (
+			<div>
+				
+				<ul class="flip minutePlay">
+					<li>
+						<a href="#">
+							<div class="up">
+								<div class="shadow"></div>
+								<div class="inn">{Math.floor(this.state.date.getHours()/10)}</div>
+							</div>
+							<div class="down">
+								<div class="shadow"></div>
+								<div class="inn">{Math.floor(this.state.date.getHours()/10)}</div>
+							</div>
+						</a>
+					</li>
+					<li>
+						<a href="#">
+							<div class="up">
+								<div class="shadow"></div>
+								<div class="inn">{Math.floor(this.state.date.getHours()/10)+1}</div>
+							</div>
+							<div class="down">
+								<div class="shadow"></div>
+								<div class="inn">{Math.floor(this.state.date.getHours()/10)+1}</div>
+							</div>
+						</a>
+					</li>
+				</ul>
+
+				<ul class="flip secondPlay">
+					<li>
+						<a href="#">
+							<div class="up">
+								<div class="shadow"></div>
+								<div class="inn">{this.state.date.getHours()%10}</div>
+							</div>
+							<div class="down">
+								<div class="shadow"></div>
+								<div class="inn">{this.state.date.getHours()%10}</div>
+							</div>
+						</a>
+					</li>
+					<li>
+						<a href="#">
+							<div class="up">
+								<div class="shadow"></div>
+								<div class="inn">{this.state.date.getHours()%10+1}</div>
+							</div>
+							<div class="down">
+								<div class="shadow"></div>
+								<div class="inn">{this.state.date.getHours()%10+1}</div>
+							</div>
+						</a>
+					</li>
+				</ul>
+				
+
+
+
+
+
+				<ul class="flip minutePlay">
+					<li>
+						<a href="#">
+							<div class="up">
+								<div class="shadow"></div>
+								<div class="inn">{Math.floor(this.state.date.getMinutes()/10)}</div>
+							</div>
+							<div class="down">
+								<div class="shadow"></div>
+								<div class="inn">{Math.floor(this.state.date.getMinutes()/10)}</div>
+							</div>
+						</a>
+					</li>
+					<li>
+						<a href="#">
+							<div class="up">
+								<div class="shadow"></div>
+								<div class="inn">{Math.floor(this.state.date.getMinutes()/10)+1}</div>
+							</div>
+							<div class="down">
+								<div class="shadow"></div>
+								<div class="inn">{Math.floor(this.state.date.getMinutes()/10)+1}</div>
+							</div>
+						</a>
+					</li>
+				</ul>
+
+				<ul class="flip secondPlay">
+					<li>
+						<a href="#">
+							<div class="up">
+								<div class="shadow"></div>
+								<div class="inn">{this.state.date.getMinutes()%10}</div>
+							</div>
+							<div class="down">
+								<div class="shadow"></div>
+								<div class="inn">{this.state.date.getMinutes()%10}</div>
+							</div>
+						</a>
+					</li>
+					<li>
+						<a href="#">
+							<div class="up">
+								<div class="shadow"></div>
+								<div class="inn">{this.state.date.getSeconds()%10+1}</div>
+							</div>
+							<div class="down">
+								<div class="shadow"></div>
+								<div class="inn">{this.state.date.getSeconds()%10+1}</div>
+							</div>
+						</a>
+					</li>
+				</ul>
+
+
+
+
+
+				<ul class="flip minutePlay">
+					<li>
+						<a href="#">
+							<div class="up">
+								<div class="shadow"></div>
+								<div class="inn">{Math.floor(this.state.date.getSeconds()/10)}</div>
+							</div>
+							<div class="down">
+								<div class="shadow"></div>
+								<div class="inn">{Math.floor(this.state.date.getSeconds()/10)}</div>
+							</div>
+						</a>
+					</li>
+					<li>
+						<a href="#">
+							<div class="up">
+								<div class="shadow"></div>
+								<div class="inn">{Math.floor(this.state.date.getSeconds()/10)+1}</div>
+							</div>
+							<div class="down">
+								<div class="shadow"></div>
+								<div class="inn">{Math.floor(this.state.date.getSeconds()/10)+1}</div>
+							</div>
+						</a>
+					</li>
+				</ul>
+
+				<ul class="flip secondPlay">
+					<li>
+						<a href="#">
+							<div class="up">
+								<div class="shadow"></div>
+								<div class="inn">{this.state.date.getSeconds()%10}</div>
+							</div>
+							<div class="down">
+								<div class="shadow"></div>
+								<div class="inn">{this.state.date.getSeconds()%10}</div>
+							</div>
+						</a>
+					</li>
+					<li>
+						<a href="#">
+							<div class="up">
+								<div class="shadow"></div>
+								<div class="inn">{this.state.date.getSeconds()%10+1}</div>
+							</div>
+							<div class="down">
+								<div class="shadow"></div>
+								<div class="inn">{this.state.date.getSeconds()%10+1}</div>
+							</div>
+						</a>
+					</li>
+				</ul>
+
+
+
+			</div>
+		);
+	}
+}
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+	<Clock />,
+	document.getElementById('root')
+	);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
